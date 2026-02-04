a111765a206a5eb1d07e3b6e23c0e40e84c6655f
kanika <kanikachauda1004@gmail.com>
2026-02-04 14:27:21 +0530

    broken forwardRef fix

--- components/ui/separator.tsx
+++ components/ui/separator.tsx
@@ -3,14 +3,11 @@ import * as SeparatorPrimitive from "@radix-ui/react-separator"
 
 import { cn } from "@/lib/utils"
 
-const Separator = React.forwardRef
+const Separator = React.forwardRef<
   React.ElementRef<typeof SeparatorPrimitive.Root>,
   React.ComponentPropsWithoutRef<typeof SeparatorPrimitive.Root>
 >(
-  (
-    { className, orientation = "horizontal", decorative = true, ...props },
-    ref
-  ) => (
+  ({ className, orientation = "horizontal", decorative = true, ...props }, ref) => (
     <SeparatorPrimitive.Root
       ref={ref}
       decorative={decorative}
@@ -24,6 +21,7 @@ const Separator = React.forwardRef
     />
   )
 )
+
 Separator.displayName = SeparatorPrimitive.Root.displayName
 
 export { Separator }
